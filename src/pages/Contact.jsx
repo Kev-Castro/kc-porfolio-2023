b11c56f8c3f7535816068d313a099fe73507a2e6
Kevin Castro <143019683+Kev-Castro@users.noreply.github.com>
2023-11-11 13:46:02 -0500

    added contact me form finshed portfolio

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,91 @@
-function Contact() {
-    return (
-        <main className="container-fluid">
-            <h1>Contact</h1>
-        </main>
-    )
-}
+// function Contact() {
+//     return (
+//         <main className="container-fluid">
+//             <h1>Contact</h1>
+//         </main>
+//     )
+// }
 
-export default Contact
+// export default Contact
+
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Contact = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Add your form submission logic here
+        console.log('Form submitted:', formData);
+    };
+
+    return (
+        <div className="container mt-5 mb-5">
+            <h2>Contact Me</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">
+                        Your Name:
+                    </label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="name"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">
+                        Your Email:
+                    </label>
+                    <input
+                        type="email"
+                        className="form-control"
+                        id="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="message" className="form-label">
+                        Your Message:
+                    </label>
+                    <textarea
+                        className="form-control"
+                        id="message"
+                        name="message"
+                        value={formData.message}
+                        onChange={handleChange}
+                        rows="4"
+                        required
+                    />
+                </div>
+
+                <button type="submit" className="btn btn-primary">
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default Contact;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,33 +2,33 @@ function Portfolio() {
     return (
         <main className="container-fluid">
             <h1>Portfolio</h1>
-            <div class="row row-cols-1 row-cols-md-3 g-4 text-center pt-4 pb-4">
-                <div class="col">
-                    <div class="card mx-auto">
-                        <img src="./images/musik.png" class="card-img-top" alt="..."></img>
-                        <div class="card-body">
-                            <h5 class="card-title">Musik</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div className="row row-cols-1 row-cols-md-3 g-4 text-center pt-4 pb-4">
+                <div className="col">
+                    <a href="https://arcane-refuge-32686-92d3868f1b08.herokuapp.com/" target="_blank" className="card mx-auto">
+                        <img src="./images/musik.png" className="card-img-top" alt="..."></img>
+                        <div className="card-body">
+                            <h5 className="card-title">Musik</h5>
+                            <p className="card-text">Musik is an app to connect users who will be able to share songs and posts with each other. We want to create a community of music lovers who have similar interests. As fellow music lovers we want to have create a place just for music. This project was built to have like minded individuals to come together and create a community. Musik brings users to meet and post about music others are listening to. We learned how to make a social media hub for only music lovers.</p>
                         </div>
-                    </div>
+                    </a>
                 </div>
-                <div class="col">
-                    <div class="card mx-auto">
-                        <img src="./images/prepAir.png" class="card-img-top" alt="..."></img>
-                        <div class="card-body">
-                            <h5 class="card-title">prepAir</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div className="col">
+                    <a href="https://kev-castro.github.io/prepAir/" target="_blank" className="card mx-auto">
+                        <img src="./images/prepAir.png" className="card-img-top" alt="..."></img>
+                        <div className="card-body">
+                            <h5 className="card-title">prepAir</h5>
+                            <p className="card-text">prepAir arms consumers with travel-related insight ahead of time ultimately allowing you to sit back, relax, and flip that out of office switch. You'll know before you go from weather statistics to surrounding towns, landmarks, and local hot spots via our interactive map.</p>
                         </div>
-                    </div>
+                    </a>
                 </div>
-                <div class="col">
-                    <div class="card mx-auto">
-                        <img src="./images/Weather_Dash.png" class="card-img-top" alt="..."></img>
-                        <div class="card-body">
-                            <h5 class="card-title">Weather App</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                <div className="col">
+                    <a href="https://kev-castro.github.io/weather_dashboard_challenge/" target="_blank" className="card mx-auto">
+                        <img src="./images/Weather_Dash.png" className="card-img-top" alt="..."></img>
+                        <div className="card-body">
+                            <h5 className="card-title">Weather App</h5>
+                            <p className="card-text">This Webpage allows those who visit the chance to look up a City and get the current weather and also the 5 day weather forecast, it will also save your recent searches and populate them as button to go back to if need be.</p>
                         </div>
-                    </div>
+                    </a>
                 </div>
             </div>
 
