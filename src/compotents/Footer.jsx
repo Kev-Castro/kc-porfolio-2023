68aa087bed4832d14fdde5cf045ace919b81dd05
Kevin Castro <143019683+Kev-Castro@users.noreply.github.com>
2023-11-13 03:10:43 -0500

    added some styles for footer and finished footer with icons and links

--- src/compotents/Footer.jsx
+++ src/compotents/Footer.jsx
@@ -1,12 +1,31 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faDiscord } from '@fortawesome/free-brands-svg-icons';
+
 function Footer() {
-    const date = new Date()
+    const date = new Date();
 
     return (
-        <footer>
-            <p>&copy; {date.getFullYear()}</p>
-            <p>Developed by Kevin Castro </p>
+        <footer className="text-center py-3">
+            <p className="footerText">&copy; {date.getFullYear()}</p>
+            <p className="footerText">
+                <span className="d-none d-sm-inline">Developed by Kevin Castro</span>
+                <span className="d-inline d-sm-none">Dev'd by Kevin Castro</span>
+            </p>
+            <div className="mt-2">
+                <a href="https://github.com/Kev-Castro" target="_blank" rel="noopener noreferrer" className="text-dark me-3">
+                    <FontAwesomeIcon icon={faGithub} />
+                </a>
+                <a href="https://linkedin.com/in/kevin-a-castro" target="_blank" rel="noopener noreferrer" className="text-dark me-3">
+                    <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+                {/* Add link for Discord icon */}
+                <a href="https://discordapp.com/users/1146083711591391383" target="_blank" rel="noopener noreferrer" className="text-dark">
+                    <FontAwesomeIcon icon={faDiscord} />
+                </a>
+            </div>
         </footer>
-    )
+    );
 }
 
-export default Footer
+export default Footer;
