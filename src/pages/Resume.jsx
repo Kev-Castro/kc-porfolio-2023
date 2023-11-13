f5d443e79b534d51d8805c6167092ed437856d9a
Kevin Castro <143019683+Kev-Castro@users.noreply.github.com>
2023-11-13 03:38:55 -0500

    added changes to resume page button-color and added line break after h5

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,12 +4,12 @@ function Resume() {
             <div className="cardResume mx-auto my-auto">
                 <div className="card-body">
                     <h5 className="card-title mt-4">My Resume</h5>
-
-                    <p className="card-text">I look forward to working alongside with you all!</p>
+                    <br />
+                    <p className="card-text">I look forward to working alongside you all!</p>
                     <hr className="my-4"></hr>
 
                     <p className="card-text">You can download my resume down below by clicking the button.</p>
-                    <a href="./images/KevinCastro.pdf" download="KevinCastro.pdf" className="btn btn-primary">Download Resume</a>
+                    <a href="./images/KevinCastro.pdf" download="KevinCastro.pdf" className="btn btn-dark">Download Resume</a>
 
                 </div>
                 <img src="./images/KC_resume_23-1.png" className="card-img-top img-fluid" alt="resume"></img>
